68a1ce7a134a141b4e9253c416054356e7e6bd38
Gabriel Lagraña <gabilagra@gmail.com>
2025-08-07 13:47:26 -0300

    Primero commit del proyecto

---
+++ src/main/java/com/ludoteca/api/LudotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ludoteca.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudotecaApiApplication.class, args);
+	}
+
+}
